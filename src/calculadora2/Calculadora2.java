62959ac3f09deda6c63865e9d03f88830f1e3ac5
CESURMADRID <CESURMADRID@W7>
2021-05-04 14:39:21 +0200

    primera version

---
+++ src/calculadora2/Calculadora2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+
+/**
+ *
+ * @author CESURMADRID
+ */
+public class Calculadora2 {
+
+   public static int suma(int a, int b){
+       return a+b;
+   }
+   public static int resta (int a, int b){
+       return a-b;
+   }
+}
